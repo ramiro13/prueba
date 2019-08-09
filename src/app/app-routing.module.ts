bed0f3b83e1e5ab859de5563fc82e6cc6333ff44
ramiro13 <ramiroperezsanz13@gmail.com>
2019-08-09 23:16:06 +0200

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './templates/contact/contact.component';
+import { HomeComponent } from './templates/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './templates/home/home.component';
+import { ContactComponent } from './templates/contact/contact.component';
+import { SliderHomeComponent } from './components/slider-home/slider-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    SliderHomeComponent
   ],
   imports: [
     BrowserModule,
